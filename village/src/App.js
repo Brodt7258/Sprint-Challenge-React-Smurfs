3dbd2da7dffb3b5ac4d6bf6dc204111a5df5ac2e
Tim Dill <brodt7258@gmail.com>
2019-06-15 14:54:43 -0400

    set up adding new smurfs

--- village/src/App.js
+++ village/src/App.js
@@ -23,13 +23,23 @@ class App extends Component {
       });
   }
 
+  postSmurf = (smurf) => {
+    axios.post('http://localhost:3333/smurfs', smurf)
+      .then(({ data }) => {
+        console.log(data);
+        this.setState({
+          smurfs: data
+        });
+      });
+  };
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm postSmurf={this.postSmurf} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
